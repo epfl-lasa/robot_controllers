625c579702e3b483413d1d2387a62ef4b3b3c757
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2019-03-25 10:51:29 +0100

    Better PassiveDS

--- src/robot_controllers/low/PassiveDS.cpp
+++ src/robot_controllers/low/PassiveDS.cpp
@@ -6,9 +6,16 @@ namespace robot_controllers {
     namespace low {
         bool PassiveDS::Init()
         {
-            passive_ds_params_.FromRobotParams(params_);
+            assert(params_.input_dim_ == params_.output_dim_);
+            // Initialize the basis matrix
+            basis_matrix_ = Eigen::MatrixXd::Random(params_.input_dim_, params_.output_dim_);
             Orthonormalize();
             AssertOrthonormalize();
+
+            // Initialize the damping matrix
+            damping_matrix_ = Eigen::MatrixXd::Zero(params_.input_dim_, params_.output_dim_);
+
+            SetParams(params_.input_dim_, params_.values_);
 
             return true;
         }
@@ -16,51 +23,45 @@ namespace robot_controllers {
         void PassiveDS::Update(const RobotState& state)
         {
             ComputeDamping();
-            output_.desired_.force_ = -passive_ds_params_.damping_matrix_ * state.velocity_ + passive_ds_params_.eig_matrix_(0, 0) * input_.desired_.velocity_;
+            output_.desired_.force_ = -damping_matrix_ * state.velocity_ + eig_matrix_(0, 0) * input_.desired_.velocity_;
         }
 
         void PassiveDS::SetParams(unsigned int dim, const std::vector<double>& eigvals)
         {
-            assert(eigvals.size() <= dim);
+            assert(eigvals.size() <= dim && eigvals.size() > 0);
 
             params_.input_dim_ = dim;
             params_.output_dim_ = dim;
 
             // Fill the eigenvalue matrix
-            passive_ds_params_.eig_matrix_ = Eigen::MatrixXd::Zero(dim, dim);
+            eig_matrix_ = Eigen::MatrixXd::Zero(dim, dim);
 
             for (size_t i = 0; i < eigvals.size(); i++)
-                passive_ds_params_.eig_matrix_(i, i) = eigvals[i];
+                eig_matrix_(i, i) = eigvals[i];
 
             for (size_t i = eigvals.size(); i < dim; i++)
-                passive_ds_params_.eig_matrix_(i, i) = eigvals.back();
-        }
-
-        void PassiveDS::SetParams(const ParamsPassiveDS& params)
-        {
-            passive_ds_params_ = params;
-            params_ = passive_ds_params_.ToRobotParams();
+                eig_matrix_(i, i) = eigvals.back();
         }
 
         void PassiveDS::Orthonormalize()
         {
-            assert(passive_ds_params_.basis_matrix_.rows() == passive_ds_params_.basis_matrix_.cols());
-            unsigned int dim = passive_ds_params_.basis_matrix_.rows();
-            passive_ds_params_.basis_matrix_.col(0).normalize();
+            assert(basis_matrix_.rows() == basis_matrix_.cols());
+            unsigned int dim = basis_matrix_.rows();
+            basis_matrix_.col(0).normalize();
             for (unsigned int i = 1; i < dim; i++) {
                 for (unsigned int j = 0; j < i; j++)
-                    passive_ds_params_.basis_matrix_.col(i) -= passive_ds_params_.basis_matrix_.col(j).dot(passive_ds_params_.basis_matrix_.col(i)) * passive_ds_params_.basis_matrix_.col(j);
-                passive_ds_params_.basis_matrix_.col(i).normalize();
+                    basis_matrix_.col(i) -= basis_matrix_.col(j).dot(basis_matrix_.col(i)) * basis_matrix_.col(j);
+                basis_matrix_.col(i).normalize();
             }
         }
 
         void PassiveDS::AssertOrthonormalize()
         {
-            uint dim = passive_ds_params_.basis_matrix_.cols();
+            uint dim = basis_matrix_.cols();
             for (int i = 0; i < dim; ++i) {
-                assert(abs(passive_ds_params_.basis_matrix_.col(i).norm() - 1.0) < FLOATEQUAL);
+                assert(abs(basis_matrix_.col(i).norm() - 1.0) < FLOATEQUAL);
                 for (int j = 0; j < i; ++j) {
-                    assert(abs(passive_ds_params_.basis_matrix_.col(i).dot(passive_ds_params_.basis_matrix_.col(j))) < FLOATEQUAL);
+                    assert(abs(basis_matrix_.col(i).dot(basis_matrix_.col(j))) < FLOATEQUAL);
                 }
             }
         }
@@ -68,8 +69,8 @@ namespace robot_controllers {
         void PassiveDS::ComputeOrthonormalBasis()
         {
             Eigen::VectorXd dir = input_.desired_.velocity_.normalized(); // or normalize
-            assert(dir.rows() == passive_ds_params_.basis_matrix_.rows());
-            passive_ds_params_.basis_matrix_.col(0) = dir;
+            assert(dir.rows() == basis_matrix_.rows());
+            basis_matrix_.col(0) = dir;
             Orthonormalize();
         }
 
@@ -79,11 +80,10 @@ namespace robot_controllers {
             if (input_.desired_.velocity_.norm() > MINSPEED)
                 ComputeOrthonormalBasis();
             // otherwise just use the last computed basis
-            passive_ds_params_.damping_matrix_ = passive_ds_params_.basis_matrix_ * passive_ds_params_.eig_matrix_ * passive_ds_params_.basis_matrix_.transpose();
+            damping_matrix_ = basis_matrix_ * eig_matrix_ * basis_matrix_.transpose();
         }
 
     } // namespace low
-
 } // namespace robot_controllers
 
 CORRADE_PLUGIN_REGISTER(PassiveDSController, robot_controllers::low::PassiveDS, "RobotControllers.AbstractController/1.0")
--- src/robot_controllers/low/PassiveDS.hpp
+++ src/robot_controllers/low/PassiveDS.hpp
@@ -6,48 +6,6 @@
 
 namespace robot_controllers {
     namespace low {
-        struct ParamsPassiveDS {
-            Eigen::MatrixXd damping_matrix_,
-                basis_matrix_,
-                eig_matrix_;
-
-            RobotParams ToRobotParams() const
-            {
-                assert(damping_matrix_.size() == basis_matrix_.size() && damping_matrix_.size() == eig_matrix_.size());
-                RobotParams p;
-
-                p.input_dim_ = damping_matrix_.rows();
-                p.output_dim_ = damping_matrix_.cols();
-                p.time_step_ = -1.; // we do not care about the time-step
-
-                unsigned int size = damping_matrix_.size() + basis_matrix_.size() + eig_matrix_.size();
-
-                if (size > 0) {
-                    p.values_.resize(size);
-
-                    Eigen::MatrixXd::Map(p.values_.data(), p.input_dim_, p.output_dim_) = damping_matrix_;
-                    Eigen::MatrixXd::Map(p.values_.data() + damping_matrix_.size(), p.input_dim_, p.output_dim_) = basis_matrix_;
-                    Eigen::MatrixXd::Map(p.values_.data() + damping_matrix_.size() + basis_matrix_.size(), p.input_dim_, p.output_dim_) = eig_matrix_;
-                }
-
-                return p;
-            }
-
-            void FromRobotParams(const RobotParams& p)
-            {
-                if (p.input_dim_ == 0 || p.output_dim_ == 0 || p.input_dim_ != p.output_dim_)
-                    return;
-
-                damping_matrix_.resize(p.input_dim_, p.output_dim_);
-                basis_matrix_.resize(p.input_dim_, p.output_dim_);
-                eig_matrix_.resize(p.input_dim_, p.output_dim_);
-
-                damping_matrix_ = Eigen::MatrixXd::Map(p.values_.data(), p.input_dim_, p.output_dim_);
-                basis_matrix_ = Eigen::MatrixXd::Map(p.values_.data() + damping_matrix_.size(), p.input_dim_, p.output_dim_);
-                eig_matrix_ = Eigen::MatrixXd::Map(p.values_.data() + damping_matrix_.size() + basis_matrix_.size(), p.input_dim_, p.output_dim_);
-            }
-        };
-
         class PassiveDS : public AbstractController {
         public:
             explicit PassiveDS(Corrade::PluginManager::AbstractManager& manager, const std::string& plugin) : AbstractController(manager, plugin)
@@ -64,14 +22,15 @@ namespace robot_controllers {
             void Update(const RobotState& state) override;
 
             void SetParams(unsigned int dim, const std::vector<double>& eigvals);
-            void SetParams(const ParamsPassiveDS& params);
 
             // SetInput  -> Inherited from AbstractController
             // GetInput  -> Inherited from AbstractController
             // GetOutput -> Inherited from AbstractController
 
         protected:
-            ParamsPassiveDS passive_ds_params_;
+            Eigen::MatrixXd damping_matrix_,
+                basis_matrix_,
+                eig_matrix_;
 
             void Orthonormalize();
 
