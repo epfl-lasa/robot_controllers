c025bbb4315a42c3a403094623f89b35ad38c0a6
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2019-07-06 19:32:02 +0200

    Use Eigen to compute rotation error

--- src/robot_controllers/utils/math.hpp
+++ src/robot_controllers/utils/math.hpp
@@ -25,9 +25,7 @@
 
 namespace robot_controllers {
     namespace utils {
-        // This is copied from: https://github.com/jrl-umi3218/SpaceVecAlg
-        double sinc_inv(double x);
-        Eigen::Vector3d rotation_error(const Eigen::Matrix3d& R_ab, const Eigen::Matrix3d& R_ac);
+        Eigen::Vector3d rotation_error(const Eigen::Matrix3d& R_desired, const Eigen::Matrix3d& R_current);
     } // namespace utils
 } // namespace robot_controllers
 
